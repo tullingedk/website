f9b941192832e60972d3dc9f8209425a71f329ed
Vilhelm Prytz <vilhelm@prytznet.se>
2020-04-18 19:36:18 +0200

    Add link to "stadgar"

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ function App() {
     <div className="App">
       <div className="App-header">
         <h1>Tullinge gymnasium datorklubb</h1>
+        <p>Sidan inte klar!</p>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.dropbox.com/s/jfhkiznd83o64t7/Tullinge_gymnasium_datorklubb_stadgar.docx?dl=0"
+        >
+          Stadgar
+        </a>
         <iframe
+          className="westworld"
           title="lol"
           width="560"
           height="315"
-          src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?controls=0"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
