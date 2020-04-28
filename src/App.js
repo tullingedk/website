93570190580cc63e24ffd25d4137602d5fe7ef79
Vilhelm Prytz <vilhelm@prytznet.se>
2020-04-28 21:17:30 +0200

    Update dependencies and remove YouTube iframe

--- src/App.js
+++ src/App.js
@@ -14,16 +14,6 @@ function App() {
         >
           Stadgar
         </a>
-        <iframe
-          className="westworld"
-          title="lol"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
       </div>
     </div>
   );
